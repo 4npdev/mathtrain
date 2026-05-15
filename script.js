30bc3fc014edb0fc7e7fcd7659a30de336ce151a
4npweb <4np.web@gmail.com>
2026-05-15 23:25:24 +0200

    Added streak counter to navbar and created HTML/CSS for the main game

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 //Variables
+const headerStreak = document.getElementById("header-streak");
 const headerBest = document.getElementById("header-best");
+let streak = Number(localStorage.getItem("streak")) || 0;
 let best = Number(localStorage.getItem("best")) || 0;
 
 //Code
-headerBest.textContent = "Highest streak: " + best;
+headerStreak.textContent = "Streak: " + streak;
+headerBest.textContent = "Best: " + best;
